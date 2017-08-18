3e89b6df6ef72ccfc698d88f27d1c1873bcb93e3
zsdostar <zsdzzydos@gmail.com>
2017-08-18 23:21:32 +0800

    抓取图片，文字等的原型制作完毕

--- tour/get_data_to_db/t_get_pic.py
+++ tour/get_data_to_db/t_get_pic.py
@@ -5,10 +5,24 @@
 import urllib
 import urllib2
 import re
+from lxml import etree
 
-imgurl = 'http://gss0.baidu.com/6b1IcTe9R1gBo1vgoIiO_jowehsv/maps/services/thumbnails?width=525&height=295&quality=100&align=middle,middle&src=http://gss0.baidu.com/7LsWdDW5_xN3otqbppnN2DJv/lvpics/pic/item/faedab64034f78f0dd0e635279310a55b3191c0d.jpg'
-urllib.urlretrieve(imgurl, r'D:\\beijing.jpg')
 
-url = 'https://lvyou.baidu.com/beijing/'
+img_url = 'http://gss0.baidu.com/6b1IcTe9R1gBo1vgoIiO_jowehsv/maps/services/thumbnails?width=525&height=295&quality=100&align=middle,middle&src=http://gss0.baidu.com/7LsWdDW5_xN3otqbppnN2DJv/lvpics/pic/item/faedab64034f78f0dd0e635279310a55b3191c0d.jpg'
+urllib.urlretrieve(img_url, r'D:\\beijing.jpg')
+
+url = 'https://lvyou.baidu.com/daqing/'
 html = urllib2.urlopen(url).read()
-re.search('', html)
+
+imgurl = re.search('//gss0.baidu.com/.+.jpg"', html).group()
+print imgurl
+
+# xpath提取文字
+html = html.decode('utf8')
+html = etree.HTML(html)
+text = html.xpath('//p[@class="main-desc-p"]/text()')
+text.remove('\n')
+text[0].replace('\n', '')
+print text[0][1:len(text[0])-1]
+
+
